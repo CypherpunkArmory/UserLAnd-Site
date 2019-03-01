adf62196f34f86ba1e565821742729abcf536120
Thomas Luong <Luongt@me.com>
2019-03-01 14:23:33 -0800

    Update jobs page to show currently needed position

--- src/views/pages/jobs/Job.js
+++ src/views/pages/jobs/Job.js
@@ -1,18 +1,32 @@
 import React from 'react';
 
-const Job = ({ title, applicationDeadline, description, requirements }) => {
+const Job = ({ title, salaryRange, description, dayToDayTasks, requirementsDescription, requirements, extraAwesomePointsDescription, extraAwesomePoints, benefits }) => {
   return (
     <li className="job">
       <div className="row">
         <div className="job__head col-lg-4">
           <h4 className="job__title">{title}</h4>
-          <h5>Application Deadline: {applicationDeadline}</h5>
+          <h5>Salary Range: {salaryRange}</h5>
         </div>
         <div className="job__body col-lg-8">
-          <p>{description}</p>
+
+          <p className="display-linebreak">{description}</p>
+          <ul className="job__requirements">
+            {dayToDayTasks.map((requirement, key) => <li key={key}>{requirement}</li>)}
+          </ul>
+          <br />
+
+          <p className="display-linebreak">{requirementsDescription}</p>
           <ul className="job__requirements">
             {requirements.map((requirement, key) => <li key={key}>{requirement}</li>)}
           </ul>
+          <br />
+          <p className="display-linebreak">{extraAwesomePointsDescription}</p>
+          <ul className="job__requirements">
+            {extraAwesomePoints.map((requirement, key) => <li key={key}>{requirement}</li>)}
+          </ul>
+          <br />
+
           <p>To apply, email your resume and cover letter to <strong><a className="link link_underline" href="mailto:jobs@userland.tech">jobs@userland.tech</a></strong></p>
         </div>
       </div>
--- src/views/pages/jobs/jobstext.js
+++ src/views/pages/jobs/jobstext.js
@@ -1,15 +1,37 @@
 export default {
     jobs: [
         {
-            title: "Software Engineer",
-            applicationDeadline: "11/15/2018",
-            description: "This software engineering position is for someone who lives and breathes Linux and is not afraid to get their hands dirty and work at the lowest levels of code.  This is for someone who might be a kernel hacker or a linux magician.",
+            title: "Platform / Systems Engineer",
+            salaryRange: "$65,000 - $110,000",
+            description: "UserLAnd/Foxhole is seeking a Systems Engineer of any experience level to help us as bring the power of Linux computing back to the people. \n\n UserLAnd is a set of open-source technologies for democratizing access to the full set of your phones computing abilities. For many people in the world, a smartphone is their primary computing device. But the smartphone is limited - it's not hackable by anyone but the most technically savvy, and it's uses are limited by the corporate interests who sold it to you. UserLAnd aims to let you Use Linux Anywhere by enabling you to run full Linux distributions on your Android device and provide a gateway to cloud computing for all users. Additionally, we are building Foxhole, a privacy focused set of common tools, on top of the Userland platform.  \n\n We are seeking a skilled Linux systems engineer to help us define and manage our online services as we roll out our cloud offerings, as well as supporting the local execution of Linux on Android and Foxhole devices. As an employee at Userland you will join a small independent team within the larger London Trust Media organization at bringing the vision of using Linux anywhere to life. \n\n\n Your day to day tasks will include:",
+            dayToDayTasks: [
+                "Helping to build custom privacy-focused Linux distribution(s) that empowers users to complete their computing tasks while keeping them free of excessive monitoring and oversight.",
+                "Maintaining and helping to build out our container infrastructure.",
+                "Advising and staying abreast of security and privacy best practices for both large cloud installations and individual user accounts. In keeping with our mission and ideals Userland/Foxhole technologies aim to be both private and secure by default.",
+                "Debugging deep and obscure problems with Linux computers, with both other developers and users.",
+                "Network / IT and system administration and configuration.",
+                "Providing on-call assistance as required."
+            ],
+            requirementsDescription: "We understand that not everybody will be versed in all of these technologies - this is a list of technologies we currently employ, not a list of buzzwords to put on your resume! Our missions is to bring Linux to everyone and believing in that mission is the most important qualification! That said, here are some skills you might use daily or weekly at Userland/Foxhole:",
             requirements: [
-                "Works in and has a strong understanding of Linux and standard distributions.",
-                "Has strong programming skills in bash and C.",
-                "Has additional programming skills in things like perl, python and such.",
-                "Communicates openly and effectively.",
-            ]
+                "Using and maintaining Linux based systems.",
+                "Hashicorp stack including Terraform, Nomad, Consul and Vault",
+                "Docker / LXC / Containers",
+                "Virtualization technologies like Virtualbox, KVM and QEMU",
+                "AWS / Cloud Computing",
+                "Networking administration including firewalls and VPNs",
+                "Ansible Configuration Management",
+                "Mail Server / CalDav / CardDAV administration.",
+                "Bash / Python / Go scripting",
+                "Datadog / Prometheus / StatsD"
+            ],
+            extraAwesomePointsDescription: "Extra awesome points for:",
+            extraAwesomePoints: [
+                "Speaking or reading Chinese, Arabic, Portugese, Hindi or Russian. Our product is international and we expect a significant fraction of users to speak a language other than English as their primary language.",
+                "People whose lived experience intersects with our mission. Didn't have a computer growing up? We want your insight!",
+                "Open-source contributions and advocacy. Userland and Foxhole are built on open-source technologies. As part of our commitment to privacy and open-source all of our supporting code will be released (eventually) as open-source."
+            ],
+            benefits: "This position is onsite at our Portland, Oregon office with the rest of the Userland team. As the rest of the team is co-located, an on-site candidate is strongly preferred. The expected salary for this position is $65K-$110K per year. London Trust Media offers a full suite of benefits including family health, vision, and dental insurance, discounted VPN memberships and more. Our office is located at the Urban Office place co-working facility in Portland, who provide in-office perks such as a gym, beer and kombucha on tap and a cool robotic barista."
         },
     ]
 }
