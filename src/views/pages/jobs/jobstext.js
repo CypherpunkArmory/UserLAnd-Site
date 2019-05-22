9aefe0bb8a3c412ccf3fdbf15cec8b64329095f6
Corbin Champion <corbinlc@gmail.com>
2019-05-22 14:22:10 -0700

    blank out the jobs page for now

--- src/views/pages/jobs/jobstext.js
+++ src/views/pages/jobs/jobstext.js
@@ -1,14 +1,11 @@
 export default {
     jobs: [
         {
-            title: "Software Engineer",
-            applicationDeadline: "11/15/2018",
-            description: "This software engineering position is for someone who lives and breathes Linux and is not afraid to get their hands dirty and work at the lowest levels of code.  This is for someone who might be a kernel hacker or a linux magician.",
+            title: "No positions open currently",
+            applicationDeadline: "TBD",
+            description: "We have currently filled all of the positions that we have funding for.  Please check back later.",
             requirements: [
-                "Works in and has a strong understanding of Linux and standard distributions.",
-                "Has strong programming skills in bash and C.",
-                "Has additional programming skills in things like perl, python and such.",
-                "Communicates openly and effectively.",
+                "TBD",
             ]
         },
     ]
