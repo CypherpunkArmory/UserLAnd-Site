250f99a47f84ef3c1d74a6a33c9f5a08b36185ba
Elbert Cortez <elbertraudry@gmail.com>
2019-06-17 17:48:15 -0500

    removes files

--- src/views/components/buttons/AppleStore.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './index.css';
-import btnApple from './btn-Apple.png';
-
-export default function AppleStore() {
-  return (
-    <span className='btn-apple' role='presentation' aria-label='apple store coming soon'><img className='img-fluid' src={btnApple} alt='App Store' /></span>
-  )
-}
